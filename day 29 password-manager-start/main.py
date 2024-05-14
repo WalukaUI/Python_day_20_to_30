8746d88111209062a2d4757ad0e24332d3f8ef8a
walukaui <cwaluka@yahoo.com>
2024-05-14 17:08:46 -0500

    "edited password manager with try except"

--- day 29 password-manager-start/main.py
+++ day 29 password-manager-start/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
@@ -29,19 +30,47 @@ def add_details():
     site_name = name_entry.get()
     email = email_entry.get()
     pw = pw_entry.get()
+    new_data = {
+        site_name : {
+            "email": email,
+            "password": pw,
+        }
+    }
+
 
     if len(site_name) == 0 or len(pw) == 0:
         messagebox.showinfo(title="Oops", message="please do not leave blanks")
     else:
         is_ok = messagebox.askokcancel(title="Verify info", message="Please verify info")
         if is_ok:
-            with open("detailsfile.txt","a") as file:
-                file.write(f"{site_name} | {email} | {pw} \n")
-            name_entry.delete(0, END)
-            name_entry.focus()
-            pw_entry.delete(0, END)
+            try:
+                with open("detailsfile.json","r") as file:
+                    #reading old data
+                    data = json.load(file)
+            except FileNotFoundError:
+                with open("detailsfile.json", "w") as file:
+                    json.dump(new_data, file, indent=4)
+            else:
+                # update old data
+                data.update(new_data)
+                with open("detailsfile.json", "w") as file:
+                    json.dump(data, file, indent=4)
+            finally:
+                name_entry.delete(0, END)
+                name_entry.focus()
+                pw_entry.delete(0, END)
 
-
+def search_key():
+    key_name = name_entry.get()
+    with open("detailsfile.json", "r") as file:
+        # reading old data
+        data = json.load(file)
+        if key_name in data:
+            emailadd = data[key_name]["email"]
+            passwords=data[key_name]["password"]
+            print(emailadd, passwords)
+        else:
+            print("no")
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -58,19 +87,21 @@ email_uname.grid(column=0, row=2)
 password = Label(text="Password :")
 password.grid(column=0, row=3)
 
-name_entry = Entry(width=35)
-name_entry.grid(column=1, row=1, columnspan=2)
+name_entry = Entry(width=21)
+name_entry.grid(column=1, row=1)
 name_entry.focus()
-email_entry = Entry(width=35)
+email_entry = Entry(width=46)
 email_entry.grid(column=1, row=2, columnspan=2)
 email_entry.insert(0, "walukaweb@yahoo.com")
 pw_entry = Entry(width=21)
 pw_entry.grid(column=1, row=3)
 
-gen_button = Button(text="Generate Password", command=generate_password)
+gen_button = Button(text="Generate Password", command=generate_password, width=20)
 gen_button.grid(column=2, row=3)
-add_button = Button(text="Add", command=add_details, width=36)
+add_button = Button(text="Add", command=add_details, width=39)
 add_button.grid(column=1, row=4, columnspan=2)
+search_btn = Button(text="Search", command=search_key, width=20)
+search_btn.grid(column=2, row=1)
 
 
 
